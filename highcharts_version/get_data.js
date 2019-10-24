f6f0972847ffc43939e0b146c23fb63aad4b9dc6
laurelmachak <laurelmachak@gmail.com>
2019-10-24 16:28:37 -0700

    data for past year, still need to implament daily averages and hourly per day

--- highcharts_version/get_data.js
+++ highcharts_version/get_data.js
@@ -17,8 +17,8 @@ async function get_data(){
 
     // data.prices : [[unixtimestamp in ms, price]]
     let proxy = 'https://cors-anywhere.herokuapp.com/';
-    // let api_link = `https://api.coingecko.com/api/v3/coins/monero/market_chart?vs_currency=usd&days=365`;
-    let api_link = `https://api.coingecko.com/api/v3/coins/monero/market_chart/range?vs_currency=usd&from=${start_date}&to=${end_date}`
+    let api_link = `https://api.coingecko.com/api/v3/coins/monero/market_chart?vs_currency=usd&days=365`;
+    // let api_link = `https://api.coingecko.com/api/v3/coins/monero/market_chart/range?vs_currency=usd&from=${start_date}&to=${end_date}`
     let response = await fetch(proxy+api_link);
     data = await response.json();
     console.log(data);    
